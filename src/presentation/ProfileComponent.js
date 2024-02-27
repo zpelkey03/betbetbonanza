2cf09ce539554e4726ba12ea12d71efa2ddcf738
Fattyffat <40011198+Fattyffat@users.noreply.github.com>
2024-02-26 20:23:45 -0700

    Minor changes

--- src/presentation/ProfileComponent.js
+++ src/presentation/ProfileComponent.js
@@ -21,6 +21,12 @@ function ProfileComponent() {
         }
     };
 
+
+    if (fetchedUser.credits != undefined) {
+        //Limit to 2 decimal places for credits
+        fetchedUser.credits = Number(fetchedUser.credits.toFixed(2));
+    }
+
     return (
         <div className="flex flex-col sticky top-0 z-10 w-4/5 mx-auto mt-8">
             <div className="bg-gray-900 border border-gray-800 shadow-lg rounded-2xl p-4">
@@ -30,9 +36,18 @@ function ProfileComponent() {
                             <div className="w-full flex-none text-1x1 text-gray-200 font-bold leading-none mt-2">{fetchedUser.firstName + " " + fetchedUser.lastName}</div>
                             <div className="flex-auto text-gray-400 my-1">
                                 <span className="mr-3">{fetchedUser.email}</span>
-                                <span className="mr-3">{"Total Credits: " + fetchedUser.credits}</span>
+                                
                             </div>
                         </div>
+
+
+                         {/* Right side box */}
+                    <div className="flex-none bg-gray-600 h-95p w-5p rounded-2xl flex flex-col items-center justify-center text-white p-5">
+                        <p className="font-bold text-lg">Credits: </p>
+                        <div className="text-2xl font-bold">
+                            {"$" + fetchedUser.credits}
+                        </div>
+                    </div>
                     </div>
                 </div>
             </div>
@@ -44,7 +59,6 @@ function ProfileComponent() {
                     {userBets.map((bet, index) => (
                         <li key={index} className="text-gray-300">
 
-                            {/* Delete the above stuff and instead call UserBetsView cpompoent */}
 
                             <UserBetsView gameInformation={bet}></UserBetsView>
                         </li>
