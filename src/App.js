86b0ecb43ab70a88762641479941ed64afb1e92c
Fattyffat <40011198+Fattyffat@users.noreply.github.com>
2024-03-26 00:07:14 -0600

    Trying refresh

--- src/App.js
+++ src/App.js
@@ -1,43 +1,83 @@
 import React from 'react';
 import "tailwindcss/tailwind.css"; 
 import LogInComponent from './presentation/LogInComponent';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useNavigate} from 'react-router-dom';
 import UserDashboard from './presentation/UserDashboard';
 import AdminDashboard from './presentation/adminComponents/AdminDashboard';
+import { getAuth, signOut, onAuthStateChanged } from "firebase/auth";
+import { useEffect } from 'react';
+
+
 
 const App = () => {
-  // const [dataFromFirebase, setDataFromFirebase] = useState([]);
+  
+  //When refresh is pressed, perform the firebase sign out function 
+  const LogoutOnRefresh = () => {
+    useEffect(() => {
+      const handleBeforeUnload = () => {
+        const auth = getAuth();
+        signOut(auth).then(() => {
+         
+        }).catch((error) => {
 
-  // const fetchData = async () => {
-  //   try {
-  //     const data = await fetchDataFromFirebase();
-  //     setDataFromFirebase(data);
-  //   } catch (error) {
-  //     console.error('Error fetching data:', error);
-  //   }
-  // };
+          // An error happened during sign-out.
+          console.error("Error during sign-out on refresh:", error);
+        });
+  
+        
+      };
+  
+      //add this event so that it always listens 
+      window.addEventListener('beforeunload', handleBeforeUnload);
+  
+      return () => {
+        window.removeEventListener('beforeunload', handleBeforeUnload);
+      };
+    }, []);
+  
+    return null; // This component does not render anything
+  };
+  
+  //This code will run all the time and monitors the users authentication status
+  //If it detects that the user is null, then it will navigate back to / 
+  const AuthenticationListener = () => {
+    const navigate = useNavigate();
+  
+    useEffect(() => {
+      const auth = getAuth();
+      const unsubscribe = onAuthStateChanged(auth, user => {
+        if (!user) {
 
-  // useEffect(() => {
-  //   // Fetch data from Firebase when the component mounts
-  //   fetchData();
-  // }, []); // Empty dependency array ensures the effect runs only once on mount
+          //redirect to login page
+          navigate("/");
+        }
+      });
+  
+      
+      return () => unsubscribe();
+    }, [navigate]);
+  
 
-  // const handleButtonClick = () => {
-  //   // Fetch data from Firebase when the button is clicked
-  //   fetchData();
-  // };
+    return null;
+  };
+
 
   return (
     <div>
       <Router>
+        
+        
+        <LogoutOnRefresh />
 
+        <AuthenticationListener />
+        
         <Routes>
           <Route path="/" element={<LogInComponent />} />
           <Route path="/dashboard" element={<UserDashboard />} />
           <Route path="/admin" element={<AdminDashboard />} />
           
         </Routes>
-
+        
       </Router>
       
       
@@ -45,5 +85,9 @@ const App = () => {
     </div>
   );
 };
+
+
+
+
 
 export default App;
